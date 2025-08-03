eb889641982fde7b2c2d6791b46cf39f08ead0f8
greenjaguarr <marksmit2002@ziggo.nl>
2025-08-03 21:54:08 +0200

    Fix bug with resetting house count of properties after owner wnet bankrupt

--- monopoly_gamelogic_async.py
+++ monopoly_gamelogic_async.py
@@ -616,6 +616,7 @@ class GameLogic_async:
             else:
                 for p in client.properties:
                     p.owner = None
+                    p.house_count = 0
                 client.properties = []
         if not_finished_count<2:
             self.finished = True
